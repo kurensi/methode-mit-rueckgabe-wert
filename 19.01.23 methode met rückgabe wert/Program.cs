ba2ce6da008799b491f704acc0c3d2bbb6b4c692
dejan <dejan@DESKTOP-HA22HU0>
2023-02-28 18:34:21 +0100

    Projektdateien hinzufügen.

---
+++ 19.01.23 methode met rückgabe wert/Program.cs
@@ -0,0 +1,12 @@
+﻿double x = 4;
+Console.WriteLine($"f(g(x)) = {f(f(g(x)))}");
+Console.WriteLine($"g(f(x)) = {g(f(x))}");
+
+static double f(double x)
+{
+    return 2 * x;
+}
+static double g(double x)
+{
+    return x * x;
+}
